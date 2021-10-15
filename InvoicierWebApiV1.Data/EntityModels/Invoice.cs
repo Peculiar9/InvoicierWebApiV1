2ad2af4cf1841e36d420b6d69b327cc924f4b475
Peculiar Babalola <omobabson@gmail.com>
2021-10-15 17:40:29 +0100

    ✅Invoice Generation feature Added

--- InvoicierWebApiV1.Data/EntityModels/Invoice.cs
+++ InvoicierWebApiV1.Data/EntityModels/Invoice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,13 @@ namespace InvoicierWebApiV1.Data.EntityModels
 {
     public class Invoice
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public bool Status { get; set; } 
+        [Required]
         public string Price { get; set; }
+        
 
     }
 }
